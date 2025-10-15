712cb19043a8105d24458c8389266a1fc5ac7f2e
S1211660 <s1211660@student.windesheim.nl>
2025-10-15 21:45:16 +0200

    feat: ProductRepository omgezet naar database
    ProductRepository is nu een SQLite database in plaats van in memory

--- Grocery.Core.Data/Repositories/ProductRepository.cs
+++ Grocery.Core.Data/Repositories/ProductRepository.cs
@@ -1,21 +1,56 @@
-﻿using Grocery.Core.Interfaces.Repositories;
+﻿using Grocery.Core.Data.Helpers;
+using Grocery.Core.Interfaces.Repositories;
 using Grocery.Core.Models;
+using Microsoft.Data.Sqlite;
 
 namespace Grocery.Core.Data.Repositories
 {
-    public class ProductRepository : IProductRepository
+    public class ProductRepository : DatabaseConnection, IProductRepository
     {
-        private readonly List<Product> products;
+        private readonly List<Product> products = [];
+
         public ProductRepository()
         {
-            products = [
-                new Product(1, "Melk", 300, new DateOnly(2025, 9, 25), 0.95m),
-                new Product(2, "Kaas", 100, new DateOnly(2025, 9, 30), 7.98m),
-                new Product(3, "Brood", 400, new DateOnly(2025, 9, 12), 2.19m),
-                new Product(4, "Cornflakes", 0, new DateOnly(2025, 12, 31), 1.48m)];
+            CreateTable(@"CREATE TABLE IF NOT EXISTS Product (
+                            [Id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                            [Name] NVARCHAR(80) NOT NULL,
+                            [Stock] INTEGER NOT NULL,
+                            [ShelfLife] DATE NOT NULL,
+                            [Price] DECIMAL(5,2) NOT NULL)");
+
+            List<string> insertQueries = [
+                @"INSERT OR IGNORE INTO Product(Id, Name, Stock, ShelfLife, Price) VALUES(1, 'Melk', 300, '2025-09-25', 0.95)",
+                @"INSERT OR IGNORE INTO Product(Id, Name, Stock, ShelfLife, Price) VALUES(2, 'Kaas', 100, '2025-09-30', 7.98)",
+                @"INSERT OR IGNORE INTO Product(Id, Name, Stock, ShelfLife, Price) VALUES(3, 'Brood', 400, '2025-09-12', 2.19)",
+                @"INSERT OR IGNORE INTO Product(Id, Name, Stock, ShelfLife, Price) VALUES(4, 'Cornflakes', 500, '2025-12-31', 1.48)"
+            ];
+            InsertMultipleWithTransaction(insertQueries);
+            GetAll();
         }
+
         public List<Product> GetAll()
         {
+            products.Clear();
+            string selectQuery = "SELECT Id, Name, Stock, date(ShelfLife), Price FROM Product";
+            OpenConnection();
+
+            using (SqliteCommand command = new(selectQuery, Connection))
+            {
+                SqliteDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0);
+                    string name = reader.GetString(1);
+                    int stock = reader.GetInt32(2);
+                    DateOnly shelfLife = DateOnly.FromDateTime(reader.GetDateTime(3));
+                    decimal price = reader.GetDecimal(4);
+
+                    products.Add(new Product(id, name, stock, shelfLife, price));
+                }
+            }
+
+            CloseConnection();
             return products;
         }
 
@@ -26,20 +61,67 @@ namespace Grocery.Core.Data.Repositories
 
         public Product Add(Product item)
         {
-            throw new NotImplementedException();
+            string insertQuery = @"INSERT INTO Product(Name, Stock, ShelfLife, Price) 
+                                   VALUES(@Name, @Stock, @ShelfLife, @Price);
+                                   SELECT last_insert_rowid();";
+            OpenConnection();
+
+            using (SqliteCommand command = new(insertQuery, Connection))
+            {
+                command.Parameters.AddWithValue("@Name", item.Name);
+                command.Parameters.AddWithValue("@Stock", item.Stock);
+                command.Parameters.AddWithValue("@ShelfLife", item.ShelfLife.ToString("yyyy-MM-dd"));
+                command.Parameters.AddWithValue("@Price", item.Price);
+
+                long newId = (long)command.ExecuteScalar();
+                item.Id = (int)newId;
+            }
+
+            CloseConnection();
+            GetAll();
+            return Get(item.Id);
         }
 
         public Product? Delete(Product item)
         {
-            throw new NotImplementedException();
+            string deleteQuery = "DELETE FROM Product WHERE Id = @Id";
+            OpenConnection();
+
+            using (SqliteCommand command = new(deleteQuery, Connection))
+            {
+                command.Parameters.AddWithValue("@Id", item.Id);
+                command.ExecuteNonQuery();
+            }
+
+            CloseConnection();
+            GetAll();
+            return item;
         }
 
         public Product? Update(Product item)
         {
-            Product? product = products.FirstOrDefault(p => p.Id == item.Id);
-            if (product == null) return null;
-            product.Id = item.Id;
-            return product;
+            string updateQuery = @"UPDATE Product 
+                                   SET Name = @Name, 
+                                       Stock = @Stock, 
+                                       ShelfLife = @ShelfLife, 
+                                       Price = @Price 
+                                   WHERE Id = @Id";
+            OpenConnection();
+
+            using (SqliteCommand command = new(updateQuery, Connection))
+            {
+                command.Parameters.AddWithValue("@Id", item.Id);
+                command.Parameters.AddWithValue("@Name", item.Name);
+                command.Parameters.AddWithValue("@Stock", item.Stock);
+                command.Parameters.AddWithValue("@ShelfLife", item.ShelfLife.ToString("yyyy-MM-dd"));
+                command.Parameters.AddWithValue("@Price", item.Price);
+
+                command.ExecuteNonQuery();
+            }
+
+            CloseConnection();
+            GetAll();
+            return Get(item.Id);
         }
     }
 }
